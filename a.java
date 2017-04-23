4315d22571b50a66386cc6f6a382828c73b3d814
227380c <1374543595@qq.com>
2017-04-23 17:38:27 +0800

    兑换纪录

--- a.java
+++ a.java
@@ -2,4 +2,10 @@ public class a{
     public stataic void main(String args){
         System.out.println("asdasdasdasds");
     }
+    public void aVoid(){
+        int i=10;
+        String name =new String("adsad");
+        System.out.print("adasd"+"     "+name);
+
+    }
 }
